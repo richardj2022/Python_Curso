dcb152b1ab0c394aeef621a04a65987a7ddb6105
serprointec <guamanzh@gmail.com>
2024-01-29 14:40:39 -0500

    Tipo de datos

---
+++ Web_Scraping/Tiposdedatos.py
@@ -0,0 +1,26 @@
+# Tipos de datos
+# print("Python tiene los tipos de datos interpretados")
+# FUNCIONES.
+# COMANDOS.
+
+# VARIABLE
+# Dato numérico
+precio = 45
+precio
+print(type(precio)) 
+# Dato Float tipo de dato numérico con decimal
+descuento = 15.4
+print(descuento)
+print(type(descuento))
+# Si utilizas mayúsculas con el mismo nombre son dos variables diferentes.
+# Precio = 20 es diferente de precio = 45
+# Tipo de dato texto string
+nombre = "Iphone"
+print(nombre)
+print(type(nombre))
+# Tipo de dato cadana de texto  
+iva = "6"
+print(iva)
+print(type(iva))
+# Tipo de datos Booleanos:  Un dato Booleano que solo puede tener dos posibles valores TRUE O FALSE
+
