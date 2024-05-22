560d45b2e3028fa2a544a1f3c981ff3a2142e646
serprointec <guamanzh@gmail.com>
2024-05-21 20:23:13 -0500

    Ejercicio Solucion

---
+++ Ejecicio_Caso_Practico/Ejercios_Solucion.py
@@ -0,0 +1,34 @@
+def AgregarCirculo():
+    radio = float(input("Ingresar el radio de la Circunferencia: "))
+
+def AgregarTriangulo():
+    lado1 = float(input("Ingresar lado # 1: "))
+    lado2 = float(input("Ingresar lado # 2: "))
+    lado3 = float(input("Ingresar lado # 3: "))
+    
+class FiguraGeometrica():
+    "Realizar calculo"    
+
+def CalculadoraFiguras():
+    "Realizar calculo"   
+
+while True:
+    print("***Que desea realizar***")
+    print("1. Agregar Círculo.")   
+    print("2. Agregar Triángulo.") 
+    print("3. Calcular Totales.") 
+    print("4. Salir.") 
+    
+    opc = input("Ingrese su opción: ")
+    
+    if opc=="1":
+        AgregarCirculo()
+    elif opc=="2":
+        AgregarTriangulo()
+    elif opc=="3":
+        pass
+    elif opc=="4":
+        pass
+    else:
+        print("Opción invalida. Intente nuevamente.")
+    
